a80e28bc9f4cbd1572a71014d0348900254ddf33
Michal Lonski <michal@lonski.pl>
2020-11-20 20:59:08 +0100

    project file and build config

---
+++ src/Paints.cs
@@ -0,0 +1,28 @@
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+[assembly: ModInfo( "Paints",
+	Description = "Vintage Story mod which adds paint preparation and coloured planks",
+	Website     = "https://github.com/lonski/vintagestory-paints",
+	Authors     = new []{ "Sps" } )]
+
+namespace Paints
+{
+	public class PaintsMod : ModSystem
+	{
+		public override void Start(ICoreAPI api)
+		{
+			
+		}
+		
+		public override void StartClientSide(ICoreClientAPI api)
+		{	
+		}
+		
+		public override void StartServerSide(ICoreServerAPI api)
+		{	
+
+		}
+	}
+}
